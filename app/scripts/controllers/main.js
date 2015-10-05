953ab6c2967542c5bf6ff9f38f518846d8f457dc
bfortuner <bfortuner@gmail.com>
2015-10-04 17:03:45 -0700

    Initialized Angular template app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vaad3dWebsiteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vaad3dWebsiteApp
+ */
+angular.module('vaad3dWebsiteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
