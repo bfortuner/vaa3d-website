0d54f929a683b0902e1617cd3be30ac9787a0622
Brendan Fortuner <fortuner@amazon.com>
2015-11-11 16:07:31 -0800

    Updated unit tests

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -8,7 +8,7 @@
  * Controller of the vaad3dWebsiteApp
  */
 vaad3dApp.controller('HomeCtrl', function () {
-    this.awesomeThings = [
+    $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
